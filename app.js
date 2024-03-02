e582d47c684bd1b22eb80a05ebd8a893c4f0e045
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-02 09:10:36 +0430

    init

--- app.js
+++ app.js
@@ -8,4 +8,18 @@ const prevBtn = document.querySelector('.prev-btn')
 container.innerHTML = people.map((person, slideIndex) => {
     const [img, name, job, text] = person
     // more logic later
+
+    return `<article class="slide">
+                <img src="https://www.course-api.com/images/people/person-3.jpeg" class="img" alt="peter doe">
+                <h4>peter doe</h4>
+                <p class="title">product designer</p>
+                <p class="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing 
+                elit. Dicta perspiciatis assumenda officia dolor
+                consequatur id obcaecati similique dolorum porro quasi!
+                </p>
+                <div class="quote-icon">
+                <div class="fas fa-quote-right"></div>
+                </div>
+             </article>`
 })
