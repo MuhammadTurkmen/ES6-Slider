38e27b0bae85fe0570b27c719158700ec5ff9d41
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-03 09:13:02 +0430

    init

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ container.innerHTML = people.map((person, slideIndex) => {
     return `<article class="slide">
                 <img src="${img}" class="img" alt="${name}">
                 <h4>${name}</h4>
-                <p class="title">product designer</p>
+                <p class="title">${job}</p>
                 <p class="text">
-                Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Dicta perspiciatis assumenda officia dolor
-                consequatur id obcaecati similique dolorum porro quasi!
+                    ${text}
                 </p>
                 <div class="quote-icon">
                 <div class="fas fa-quote-right"></div>
