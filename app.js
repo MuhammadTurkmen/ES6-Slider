a2c8de88cd1a79435d198944e4f5464d304cf3e1
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-03 09:22:32 +0430

    init

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ container.innerHTML = people.map((person, slideIndex) => {
     if(slideIndex === 0 ) {
         position = 'active'
     }
-    if(slideIndex === people.length - 1)
+    if(slideIndex === people.length - 1) {
+        position = 'last'
+    }
     return `<article class="slide ${position}">
                 <img src="${img}" class="img" alt="${name}">
                 <h4>${name}</h4>
